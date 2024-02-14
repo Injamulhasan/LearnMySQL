04a922dfcbe7587159a8290de4b26b3671524a4a
Injamul Hasan <injamulhasanakash@yahoo.com>
2024-02-14 22:52:23 +0600

    starts exploreing shows db with sqlite3

---
+++ CS50-SQL/mydb.sql
@@ -0,0 +1,33 @@
+.help
+.tables
+.schema 
+.show
+.vfslist
+.schema writers
+.schema stars
+.schema shows
+
+
+SELECT * FROM shows WHERE title = "The Office";
+SELECT * FROM shows WHERE title = "The Office" and year = 2005;
+
+SELECT title FROM shows where id IN (SELECT show_id FROM stars WHERE person_id= (SELECT id FROM people WHERE name = "Steve Carell"));
+
+SELECT title FROM shows 
+JOIN stars on shows.id=stars.show_id 
+JOIN people on stars.person_id = people.id; 
+
+SELECT title FROM shows 
+JOIN stars on shows.id=stars.show_id 
+JOIN people on stars.person_id = people.id
+WHERE name = 'Steve Carell';
+
+
+SELECT title FROM shows ,stars,people 
+WHERE shows.id =stars.show_id 
+and people.id = stars.person_id 
+and name = 'Steve Carell';
+
+
+.timer ON
+SELECT * FROM shows WHERE title = 'The Office';
